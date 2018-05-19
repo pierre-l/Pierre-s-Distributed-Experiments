fdb05558faef441cc81c9d663f921f0d65349a4c
pierre-l <pierre.larger@gmail.com>
2018-05-19 17:09:08 +0200

    Write an example of a network of randomly connected nodes.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,88 @@
 extern crate futures;
 extern crate tokio;
+extern crate rand;
 
 mod node;
 
-use node::MPSCNode;
+use node::{MPSCNode, MPSCAddress, MPSCConnection};
 use futures::future;
 use futures::Future;
 use futures::Stream;
 use std::time::Duration;
+use rand::Rng;
 
 #[derive(Clone)]
 pub struct Message{}
 
 fn main() {
-    let mut nodes = vec![];
-    let mut addresses = vec![];
-    for i in 0..2 {
-        let node = MPSCNode::new(i);
-        addresses.push(node.address().clone());
-        nodes.push(node);
-    }
-
-    for node in &mut nodes{
-        for seed in &addresses{
-            if node.address().ne(seed){
-                node.include_seed(seed.clone());
-            }
-        }
-    }
-
+    // TODO Use the tokio runtime instead of a thread per node.
+    let nodes = new_network(20, 3);
+    let mut handles = vec![];
     for node in nodes{
         println!("Starting a new node.");
-        node.run(|connection|{
+        let handle = node.run(|connection: MPSCConnection<Message>|{
             println!("Connection received.");
             let (sender, receiver) = connection.split();
 
             node::send_or_panic(&sender, Message{});
 
-            receiver
+            let reception = receiver
                 .for_each(|_message|{
                     println!("Message received.");
                     future::ok(())
                 })
                 .map_err(|_|{
                     panic!()
-                })
+                });
+
+            reception
         });
+
+        handles.push(handle);
     }
 
-    std::thread::sleep(Duration::from_millis(10));
+    std::thread::sleep(Duration::from_millis(1000));
+
+    for handle in handles{
+        drop(handle);
+    }
+}
+
+fn new_network(size: usize, average_number_of_connections_per_node: usize) -> Vec<MPSCNode<Message>>{
+    // TODO Avoid connecting the same nodes twice
+    let mut nodes = vec![];
+    let mut addresses = vec![];
+
+    for i in 0..size {
+        let node = MPSCNode::new(i);
+        addresses.push(node.address().clone());
+        nodes.push(node);
+    }
+
+    for node in &mut nodes{
+        let mut addresses = addresses.clone();
+        for _i in 0..average_number_of_connections_per_node/2 + 1 {
+            let seed_index = node.random_different_address(&addresses);
+
+            node.include_seed(addresses.remove(seed_index));
+        }
+    }
+
+    nodes
+}
+
+impl MPSCNode<Message>{
+    fn random_different_address(&self, pool: &Vec<MPSCAddress<Message>>) -> usize{
+        let mut rng = rand::thread_rng();
+
+        let mut candidate_index = rng.gen_range(0, pool.len());
+        let mut candidate = pool.get(candidate_index).unwrap().clone();
+
+        while candidate.eq(self.address()) {
+            candidate_index = rng.gen_range(0, pool.len());
+            candidate = pool.get(candidate_index).unwrap().clone();
+        }
+
+        candidate_index
+    }
 }
--- src/node.rs
+++ src/node.rs
@@ -7,13 +7,15 @@ use std::hash::Hasher;
 use std::thread;
 use futures::Stream;
 use futures::Future;
+use std::thread::JoinHandle;
 
+#[derive(Debug)]
 enum TransportMessage<M> {
-    Init(MPSCAddress<M>),
+    Init(MPSCAddress<M>, UnboundedSender<M>),
     Ack(usize, UnboundedSender<M>),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MPSCAddress<M>{
     transport_sender: UnboundedSender<TransportMessage<M>>,
     id: usize, // Necessary for PartialEq
@@ -76,41 +78,47 @@ impl <M> MPSCNode<M> where M: Clone + Send + 'static{
         self.seeds.push(address);
     }
 
-    pub fn run<A, F>(self, connection_consumer: F)
+    pub fn run<A, F>(self, connection_consumer: F) -> JoinHandle<()>
         where
     A: Future<Item=(), Error=()> + 'static,
     F: Fn(MPSCConnection<M>) -> A + Sync + Send + 'static{
-        for address in &self.seeds {
-            let init_message = TransportMessage::Init(self.address.clone());
+        let self_address = self.address;
+        let self_address_id = self_address.id;
+        let mut connections = HashMap::new();
 
-            send_or_panic(&address.transport_sender, init_message);
+        for remote_address in &self.seeds {
+            let (
+                connection_sender,
+                connection_receiver,
+            ): (UnboundedSender<M>, UnboundedReceiver<M>) = mpsc::unbounded::<M>();
+            connections.insert(remote_address.id, connection_receiver);
+
+            let init_message = TransportMessage::Init(self_address.clone(), connection_sender);
+
+            send_or_panic(&remote_address.transport_sender, init_message);
         }
 
-        let self_address_id = self.address.id;
-        let mut connections = HashMap::new();
         let node_future = self.transport_receiver.for_each(move |transport_message|{
             match transport_message {
-                TransportMessage::Init(remote_address) => {
-                    let (
-                        connection_sender,
-                        connection_receiver,
-                    ): (UnboundedSender<M>, UnboundedReceiver<M>) = mpsc::unbounded::<M>();
+                TransportMessage::Init(remote_address, remote_connection_sender) => {
+                    println!("Initiating connection from {} to {}", &remote_address.id, &self_address_id);
+
+                    let connection_sender = MPSCNode::init_new_virtual_connection(remote_connection_sender, &connection_consumer);
 
                     let ack_message = TransportMessage::Ack(self_address_id, connection_sender);
-                    connections.insert(remote_address.id, connection_receiver);
-
                     send_or_panic(&remote_address.transport_sender, ack_message);
                 },
                 TransportMessage::Ack(address_id, sender) => {
+                    println!("Ack connection from {} to {}", &self_address_id, &address_id);
                     if let Some(receiver) = connections.remove(&address_id){
                         let connection = MPSCConnection{
                             sender,
-                            receiver
+                            receiver,
                         };
 
                         current_thread::spawn(connection_consumer(connection));
                     } else {
-                        panic!()
+                        panic!("Could not find the connection to acknowledge.")
                     }
                 },
             };
@@ -126,12 +134,90 @@ impl <M> MPSCNode<M> where M: Clone + Send + 'static{
 
         thread::spawn(move || {
             current_thread::block_on_all(node_future).unwrap_or(());
-        });
+        })
+    }
+
+    fn init_new_virtual_connection<A, F>(remote_connection_sender: UnboundedSender<M>, connection_consumer: &F)
+        -> UnboundedSender<M>
+        where
+            A: Future<Item=(), Error=()> + 'static,
+            F: Fn(MPSCConnection<M>) -> A + Sync + Send + 'static
+    {
+        let (
+            connection_sender,
+            connection_receiver,
+        ): (UnboundedSender<M>, UnboundedReceiver<M>) = mpsc::unbounded::<M>();
+
+        let connection = MPSCConnection{
+            sender: remote_connection_sender,
+            receiver: connection_receiver,
+        };
+
+        current_thread::spawn(connection_consumer(connection));
+
+        connection_sender
     }
 }
 
 pub fn send_or_panic<M>(sender: &UnboundedSender<M>, message: M){
     if let Err(_err) = sender.unbounded_send(message){
-        panic!()
+        panic!("{}", _err)
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use std::time::Duration;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
+
+    #[derive(Clone, Debug)]
+    pub struct Message{}
+
+    #[test]
+    fn can_connect_2_nodes_together(){
+        let mut nodes = vec![];
+
+        for _i in 0..2{
+            let mut node: MPSCNode<Message> = MPSCNode::new(1);
+            nodes.push(node);
+        }
+
+        let node_b_address = nodes.get(1).unwrap().address.clone();
+
+        {
+            let node_a = nodes.get_mut(0).unwrap();
+
+            node_a.include_seed(node_b_address);
+        }
+
+
+        let mut handles = vec![];
+        let global_number_of_received_messages = Arc::new(AtomicUsize::new(0));
+        for node in nodes{
+            let received_messages = global_number_of_received_messages.clone();
+            let handle = node.run(move |connection|{
+                let received_messages = received_messages.clone();
+                let (sender, receiver) = connection.split();
+
+                // Send one message per connection received for each node.
+                send_or_panic(&sender, Message{});
+
+                receiver
+                    .for_each(move |_message|{
+                        println!("Message received.");
+                        received_messages.fetch_add(1, Ordering::Relaxed);
+                        future::ok(())
+                    })
+                    .map_err(|_|{
+                        panic!()
+                    })
+            });
+            handles.push(handle);
+        }
+
+        thread::sleep(Duration::from_millis(100));
+        assert_eq!(2, global_number_of_received_messages.load(Ordering::Relaxed))
     }
 }
