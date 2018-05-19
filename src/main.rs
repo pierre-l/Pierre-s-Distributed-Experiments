7f1236689e20043be6b6d8dcb98167fee99aed4e
pierre-l <pierre.larger@gmail.com>
2018-05-19 20:01:50 +0200

    Move the network related stuff to a dedicated module.

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,20 @@ extern crate futures;
 extern crate tokio;
 extern crate rand;
 
-mod node;
+mod network;
 
-use node::{MPSCNode, MPSCAddress, MPSCConnection};
+use network::node::MPSCConnection;
 use futures::future;
 use futures::Future;
 use futures::Stream;
 use std::time::Duration;
-use rand::Rng;
 
 #[derive(Clone)]
 pub struct Message{}
 
 fn main() {
     // TODO Use the tokio runtime instead of a thread per node.
-    let nodes = new_network(20, 3);
+    let nodes = network::new_network(20, 3);
     let mut handles = vec![];
     for node in nodes{
         println!("Starting a new node.");
@@ -24,7 +23,7 @@ fn main() {
             println!("Connection received.");
             let (sender, receiver) = connection.split();
 
-            node::send_or_panic(&sender, Message{});
+            network::node::send_or_panic(&sender, Message{});
 
             let reception = receiver
                 .for_each(|_message|{
@@ -45,44 +44,5 @@ fn main() {
 
     for handle in handles{
         drop(handle);
-    }
-}
-
-fn new_network(size: usize, average_number_of_connections_per_node: usize) -> Vec<MPSCNode<Message>>{
-    // TODO Avoid connecting the same nodes twice
-    let mut nodes = vec![];
-    let mut addresses = vec![];
-
-    for i in 0..size {
-        let node = MPSCNode::new(i);
-        addresses.push(node.address().clone());
-        nodes.push(node);
-    }
-
-    for node in &mut nodes{
-        let mut addresses = addresses.clone();
-        for _i in 0..average_number_of_connections_per_node/2 + 1 {
-            let seed_index = node.random_different_address(&addresses);
-
-            node.include_seed(addresses.remove(seed_index));
-        }
-    }
-
-    nodes
-}
-
-impl MPSCNode<Message>{
-    fn random_different_address(&self, pool: &Vec<MPSCAddress<Message>>) -> usize{
-        let mut rng = rand::thread_rng();
-
-        let mut candidate_index = rng.gen_range(0, pool.len());
-        let mut candidate = pool.get(candidate_index).unwrap().clone();
-
-        while candidate.eq(self.address()) {
-            candidate_index = rng.gen_range(0, pool.len());
-            candidate = pool.get(candidate_index).unwrap().clone();
-        }
-
-        candidate_index
     }
 }
---
+++ src/network/mod.rs
@@ -0,0 +1,44 @@
+pub mod node;
+
+pub use network::node::MPSCNode;
+pub use network::node::MPSCAddress;
+use rand::{self, Rng};
+
+pub fn new_network<M>(size: usize, average_number_of_connections_per_node: usize) -> Vec<MPSCNode<M>> where M: Clone + Send + 'static{
+    // TODO Avoid connecting the same nodes twice
+    let mut nodes = vec![];
+    let mut addresses = vec![];
+
+    for i in 0..size {
+        let node = MPSCNode::new(i);
+        addresses.push(node.address().clone());
+        nodes.push(node);
+    }
+
+    for node in &mut nodes{
+        let mut addresses = addresses.clone();
+        for _i in 0..average_number_of_connections_per_node/2 + 1 {
+            let seed_index = node.random_different_address(&addresses);
+
+            node.include_seed(addresses.remove(seed_index));
+        }
+    }
+
+    nodes
+}
+
+impl <M> MPSCNode<M> where M: Clone + Send + 'static{
+    fn random_different_address(&self, pool: &Vec<MPSCAddress<M>>) -> usize{
+        let mut rng = rand::thread_rng();
+
+        let mut candidate_index = rng.gen_range(0, pool.len());
+        let mut candidate = pool.get(candidate_index).unwrap().clone();
+
+        while candidate.eq(self.address()) {
+            candidate_index = rng.gen_range(0, pool.len());
+            candidate = pool.get(candidate_index).unwrap().clone();
+        }
+
+        candidate_index
+    }
+}
