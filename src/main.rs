9422d000546d01b28363c7800eaa634c73ad4cbc
pierre-l <pierre.larger@gmail.com>
2018-06-17 02:22:05 +0200

    #1 Use clap in order to provide a richer CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate env_logger;
 extern crate futures;
 #[macro_use] extern crate log;
@@ -6,36 +7,86 @@ extern crate ring;
 extern crate tokio;
 extern crate tokio_timer;
 
+use blockchain::{Chain, Difficulty, PowNode};
+use clap::{App, Arg, SubCommand};
+use network::Network;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+
 mod network;
 mod blockchain;
 mod flattenselect;
 
-use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::time::Duration;
-use blockchain::{PowNode, Chain, Difficulty};
-use network::Network;
-
 fn main() {
     env_logger::init();
 
+    let matches = App::new("The Blockchain Network Simulation")
+        .version("0.1")
+        .author("Pierre L. <pierre.larger@gmail.com>")
+        .about("Simulates a Proof-of-Work blockchain network")
+        .arg(Arg::with_name("number_of_nodes")
+            .short("n")
+            .long("network_size")
+            .value_name("NUMBER_OF_NODES")
+            .help("Defines the size of the network.")
+            .takes_value(true))
+        .arg(Arg::with_name("initiated_connections_per_node")
+            .short("c")
+            .long("connections")
+            .value_name("INITIATED_CONNECTIONS_PER_NODE")
+            .takes_value(true))
+        .arg(Arg::with_name("difficulty_factor")
+            .short("d")
+            .long("difficulty")
+            .value_name("DIFFICULTY_FACTOR")
+            .help("Number of times the minimum difficult is doubled")
+            .takes_value(true))
+        .arg(Arg::with_name("duration_in_seconds")
+            .short("s")
+            .long("duration_in_seconds")
+            .value_name("DURATION_IN_SECONDS")
+            .help("The duration of the simulation in seconds.")
+            .takes_value(true))
+        .get_matches();
+
+    let number_of_nodes: u8 = matches
+        .value_of("number_of_nodes")
+        .unwrap_or("8")
+        .parse().expect("Invalid number of nodes, expected [1-255]");
+
+    let initiated_connections_per_node: u8 = matches
+        .value_of("initiated_connections_per_node")
+        .unwrap_or("2")
+        .parse().expect("Invalid number of initiated connections per node, expected [1-255]");
+
+    let difficulty_factor: u8 = matches
+        .value_of("difficulty_factor")
+        .unwrap_or("7")
+        .parse().expect("Invalid difficulty factor, expected [1-255]");
+
+    let duration_in_seconds: u64 = matches
+        .value_of("duration_in_seconds")
+        .unwrap_or("30")
+        .parse().expect("Invalid duration in seconds, expected [1-9,223,372,036,854,775,807]");
+
     pow_network_simulation(
-        8,
-        2,
-        7,
-        Duration::from_secs(15),
+        number_of_nodes,
+        initiated_connections_per_node,
+        difficulty_factor,
+        Duration::from_secs(duration_in_seconds),
     )
 }
 
 pub fn pow_network_simulation(
     number_of_nodes: u8,
-    initiated_connections_per_node: usize,
-    difficulty_factor: usize,
+    initiated_connections_per_node: u8,
+    difficulty_factor: u8,
     duration: Duration,
 ){
     // Set up a chain.
     let mut difficulty = Difficulty::min_difficulty();
-    for _i in 0..difficulty_factor{
+    for _i in 0u8..difficulty_factor{
         difficulty.increase();
     }
 
