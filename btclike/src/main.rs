57fbdb6e51b29fe790b1296d643cb27ece615775
pierre-l <pierre.larger@gmail.com>
2018-07-06 22:04:01 +0200

    Create the btclike workspace member

---
+++ btclike/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use log::LevelFilter;
+
+fn main() {
+    // Always print backtrace on panic.
+    ::std::env::set_var("RUST_BACKTRACE", "1");
+
+    env_logger::Builder::from_default_env()
+        .default_format_module_path(false)
+        .filter_level(LevelFilter::Info)
+        .init();
+
+    info!("Hello world.")
+}
